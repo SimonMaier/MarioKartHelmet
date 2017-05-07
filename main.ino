acdf98f35951e43b729db9620515aebe9db689e8
chrisittner <mail@chrisittner.de>
2017-05-08 00:12:19 +0200

    used #define, fixed formatting

--- main.ino
+++ main.ino
@@ -1,46 +1,32 @@
-
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
+// for both classes must be in the include path of the project
 #include "I2Cdev.h"
 #include "MPU6050.h"
-#include <Keyboard.h>
+#include "Keyboard.h"
 #include "Wire.h"
-
-MPU6050 accelgyro;  // default I2C address is 0x68
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
-
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
 
 #define LED_PIN 13
 bool blinkState = false;
 
-int LEFT_THRESHHOLD = -7000;
-int LEFT_THRESHHOLD_RELEASE = -6500;
-int RIGHT_THRESHHOLD = 7000;
-int RIGHT_THRESHHOLD_RELEASE = 6500;
-int SPEED_TRESHHOLD = -6000;
-int SLOW_THRESHHOLD = -2000;
+#define LEFT_THRESHHOLD          -7000
+#define LEFT_THRESHHOLD_RELEASE  -6500
+#define RIGHT_THRESHHOLD          7000
+#define RIGHT_THRESHHOLD_RELEASE  6500
+#define SPEED_TRESHHOLD          -6000
+#define SLOW_THRESHHOLD          -2000
 
-char KEY_LEFT_ARROW_1 = 'j';
-char KEY_RIGHT_ARROW_1 = 'l';
-char KEY_UP_ARROW_1 = 'i';
-char KEY_DOWN_ARROW_1 = 'k';
-
+MPU6050 accelgyro;  // I2C address is 0x68
+int16_t ax, ay, az, gx, gy, gz;
 
 void setup() {
-    Wire.begin();  // join I2C bus
+    pinMode(LED_PIN, OUTPUT);
+    Wire.begin();
     Keyboard.begin();
-
-    Serial.begin(38400);
-
-    Serial.println("Initializing I2C devices...");
     accelgyro.initialize();
 
-    Serial.println("Testing device connections...");
+    Serial.begin(115200);
+    Serial.println("Testing I2C connection...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-
-    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
@@ -49,7 +35,7 @@ void loop() {
 
     // trigger keypresses according to the sensor values
     perform_controls(ax, ay, az, gx, gy, gz);
-    reset_controls(ax, ay, az, gx, gy, gz);
+    release_controls(ax, ay, az, gx, gy, gz);
     shoot_item(gx, gy, gz);
 
     Serial.print("a/g:\t");
@@ -66,18 +52,15 @@ void loop() {
 
 
 void perform_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz) {
-    // check if the helmet is tilted to the left
+    // Trigger keypresses in case helmet is tilted to left, right, front, or back.
     if (ay <  LEFT_THRESHHOLD) {
         Keyboard.press(KEY_LEFT_ARROW);
         Serial.println("Moving Left");
     }
-    // check if the helmet is tilted to the right
     else if (ay > RIGHT_THRESHHOLD) {
         Keyboard.press(KEY_RIGHT_ARROW);
         Serial.println("Moving Right");
     }
-      
-    // Speed up if the helmet is tilted to the front
     if (ax < SPEED_TRESHHOLD) {
         Keyboard.press(KEY_LEFT_SHIFT);
         Serial.println("Speeding");
@@ -88,23 +71,23 @@ void perform_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy
     }
 }
 
-void reset_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz) {
-    // check if the speed has to be reset
+void release_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz) {
+    // release pressed keys when tilt back in normal range
     if (ay > LEFT_THRESHHOLD_RELEASE && ay < RIGHT_THRESHHOLD_RELEASE) {
         Keyboard.release(KEY_RIGHT_ARROW);
         Keyboard.release(KEY_LEFT_ARROW);
-        Serial.println("Reset");
+        Serial.println("Release Y");
     }
     if (ax > SPEED_TRESHHOLD && ax < SLOW_THRESHHOLD) {
         Keyboard.release(KEY_LEFT_SHIFT);
         Keyboard.release(KEY_LEFT_CTRL);
-        Serial.println("Reset");
+        Serial.println("Release X");
     }
 }
 
 void shoot_item(int16_t gx, int16_t gy, int16_t gz) {
     if (abs(gy) > 7000) {
-      Keyboard.write('y');
-      Serial.println("Shooting");
+        Keyboard.write('y');
+        Serial.println("Shooting");
     }
 }
