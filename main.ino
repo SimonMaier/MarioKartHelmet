d20ff401dcbf681638dc67959651f71d7a8d93f8
Tobias Duemmling <duemmlingcip.ifi.lmu.de>
2017-05-07 18:35:27 +0200

    working Version; final SWH

--- main.ino
+++ main.ino
@@ -67,10 +67,19 @@ int16_t gx, gy, gz;
 #define LED_PIN 13
 bool blinkState = false;
 
-int LEFT_THRESHHOLD = -6000;
-int RIGHT_THRESHHOLD = 6000;
-int SPEED_TRESHHOLD = - 6000;
-int SLOW_THRESHHOLD = 4000;
+int LEFT_THRESHHOLD = - 4000;
+int RIGHT_THRESHHOLD = 4000;
+int SPEED_TRESHHOLD = - 4000;
+int SLOW_THRESHHOLD = - 2000;
+
+
+int LEFT_THRESHHOLD_RELEASE = -3000;
+int RIGHT_THRESHHOLD_RELEASE = 3000;
+
+char KEY_LEFT_ARROW_1 = 'j';
+char KEY_RIGHT_ARROW_1 = 'l';
+char KEY_UP_ARROW_1 = 'i';
+char KEY_DOWN_ARROW_1 = 'k';
 
 
 void setup() {
@@ -127,7 +136,9 @@ void loop() {
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
     // calling the method that handeles the controlls according to the sensor values
-    perform_controls(ax, ay, az, gx, gy, gz); 
+    perform_controls(ax, ay, az, gx, gy, gz);
+    reset_controls(ax, ay, az, gx, gy, gz);
+    shoot_item(gx, gy, gz);
     
     // these methods (and a few others) are also available
     //accelgyro.getAcceleration(&ax, &ay, &az);
@@ -136,9 +147,6 @@ void loop() {
     #ifdef OUTPUT_READABLE_ACCELGYRO
         // display tab-separated accel/gyro x/y/z values
         Serial.print("a/g:\t");
-        Serial.print(ax); Serial.print("\t");
-        Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
         Serial.print(gx); Serial.print("\t");
         Serial.print(gy); Serial.print("\t");
         Serial.println(gz);
@@ -162,31 +170,47 @@ void perform_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy
     // check if the helmet is tilted to the left
     if ( ay <  LEFT_THRESHHOLD) {
         Keyboard.press(KEY_LEFT_ARROW);
+        Serial.println("Moving Left");
       }
     // check if the helmet is tilted to the right
     else if ( ay > RIGHT_THRESHHOLD ) {
         Keyboard.press(KEY_RIGHT_ARROW);
+        Serial.println("Moving Right");
       }
       
     // Speed up if the helmet is tilted to the front
     if ( ax < SPEED_TRESHHOLD ) {
-        Keyboard.press(KEY_UP_ARROW);
+        Keyboard.press(KEY_LEFT_SHIFT);
+        Serial.println("Speeding");
       }
     else if ( ax > SLOW_THRESHHOLD ) {
-        Keyboard.press(KEY_DOWN_ARROW);
+        Keyboard.press(KEY_LEFT_CTRL);
+        Serial.println("Slowing down");
       }
   }
 
 void reset_controls(int16_t ax, int16_t ay, int16_t az, int16_t gx, int16_t gy, int16_t gz) {
     // check if the speed has to be reset
-    if ( ay > LEFT_THRESHHOLD && ay > RIGHT_THRESHHOLD){
-        Keyboard.release(KEY_LEFT_ARROW);
+    if ( ay > LEFT_THRESHHOLD_RELEASE && ay < RIGHT_THRESHHOLD_RELEASE){
         Keyboard.release(KEY_RIGHT_ARROW);
+        Keyboard.release(KEY_LEFT_ARROW);
+        Serial.println("Resett");
       }
-    if ( ax > SPEED_TRESHHOLD && ax > SPEED_TRESHHOLD ) {
-        Keyboard.release(KEY_UP_ARROW);
-        Keyboard.release(KEY_DOWN_ARROW);
+    if ( ax > SPEED_TRESHHOLD && ax < SLOW_THRESHHOLD ) {
+        Keyboard.release(KEY_LEFT_SHIFT);
+        Keyboard.release(KEY_LEFT_CTRL);
+        Serial.println("Resett");
       }
   }
+
+void shoot_item(int16_t gx,int16_t gy,int16_t gz) {
+      int checkSum = abs(gy);
+      Serial.println(checkSum);
+    if (checkSum > 7000){
+      Keyboard.write('y');
+      Serial.println("Shooting");
+    }
+  }
+
 
   
