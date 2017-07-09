edf10813319eb3f6a5031f25535bf83690989a1f
chrisittner <mail@chrisittner.de>
2017-07-09 18:16:46 +0200

    typo

--- examples/MarioKartHelmet/mupen64plus-setup/run-mario-kart.sh
+++ examples/MarioKartHelmet/mupen64plus-setup/run-mario-kart.sh
@@ -1,1 +1,1 @@
-sudo mupen64plus --fullscreen --gfx mupen64plus-video-glide64 Mario\ Kart\ 64\ \(Europe\).n64
+sudo mupen64plus --fullscreen --gfx mupen64plus-video-glide64mk2 Mario\ Kart\ 64\ \(Europe\).n64
